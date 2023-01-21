0015cbc64a2a997eee921ff22d950fa175f99322
ncmontanar <nelson.camilo@gmail.com>
2023-01-21 12:53:44 +0100

    primer einssayo de version 1 de mng

---
+++ src/test/java/com/hosp/mng/MngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosp.mng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
